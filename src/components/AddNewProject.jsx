e6d3fd6cdf38f35206ccb81cd626420e78ab4366
TriBhaskar <panthribhaskar@gmail.com>
2023-12-05 20:53:47 +0530

    added state and refs

--- src/components/AddNewProject.jsx
+++ src/components/AddNewProject.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function AddNewProject() {
+export default function AddNewProject({ onAdd }) {
+  const titleRef = useRef();
+  const descRef = useRef();
+  const dueDateRef = useRef();
+
+  function handleSave() {
+    const enteredTitle = titleRef.current.value;
+    const enteredDesciption = descRef.current.value;
+    const enteredDueDate = dueDateRef.current.value;
+
+    //validation...
+    onAdd({
+      title: enteredTitle,
+      description: enteredDesciption,
+      dueDate: enteredDueDate,
+    });
+  }
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,15 +27,18 @@ export default function AddNewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-slate-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 py-2 rounded-md bg-slate-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input type="text" ref={titleRef} label="Title" />
+        <Input ref={descRef} label="Description" textarea />
+        <Input type="date" ref={dueDateRef} label="Due Date" />
       </div>
     </div>
   );
