0c6cb31c67bead00f4802106153a0b9887ba0ea0
TriBhaskar <panthribhaskar@gmail.com>
2023-12-06 21:08:15 +0530

    Added Modal and some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -19,31 +19,47 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
 
-  console.log(projectsState);
-
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <AddNewProject onAdd={handleAddProject} />;
+    content = (
+      <AddNewProject
+        onAdd={handleAddProject}
+        onCancel={handleCancelAddProject}
+      />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBarComponent onStartAddProject={handleStartAddProject} />
+      <SideBarComponent
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
--- src/components/AddNewProject.jsx
+++ src/components/AddNewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function AddNewProject({ onAdd }) {
+export default function AddNewProject({ onAdd, onCancel }) {
+  const modalRef = useRef();
   const titleRef = useRef();
   const descRef = useRef();
   const dueDateRef = useRef();
@@ -12,6 +14,14 @@ export default function AddNewProject({ onAdd }) {
     const enteredDueDate = dueDateRef.current.value;
 
     //validation...
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDesciption.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modalRef.current.open();
+      return;
+    }
     onAdd({
       title: enteredTitle,
       description: enteredDesciption,
@@ -19,27 +29,43 @@ export default function AddNewProject({ onAdd }) {
     });
   }
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-slate-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={titleRef} label="Title" />
-        <Input ref={descRef} label="Description" textarea />
-        <Input type="date" ref={dueDateRef} label="Due Date" />
+    <>
+      <Modal ref={modalRef} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">
+          Invalid Input
+        </h2>
+        <p className="text-stone-600 mb-4">
+          OOps looks like you forgot to enter value
+        </p>
+        <p className="text-stone-600 mb-4">
+          please make sure you provide a valid input for field
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-slate-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={titleRef} label="Title" />
+          <Input ref={descRef} label="Description" textarea />
+          <Input type="date" ref={dueDateRef} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
